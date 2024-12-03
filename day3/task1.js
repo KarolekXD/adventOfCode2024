2dd34305d164e4b1b9f1106172318dbf48b307cc
Czar0l <119510854+KarolekXD@users.noreply.github.com>
2024-12-03 18:53:48 +0100

    Update task1.js

--- day3/task1.js
+++ day3/task1.js
@@ -1,16 +1,24 @@
 const fs = require('node:fs');
 const inputData = fs.readFileSync('data.txt').toString();
 
-let regExp = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
+let proceed = true
+
+let regExp = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g;
 let checkForNumbers = /[0-9]{1,3}/g;
 
 let passingResults = [...inputData.matchAll(regExp)];
 
 let sum = 0;
 passingResults.forEach((passingresult) => {
-  let currentNumbers = [];
-  currentNumbers = [...passingresult[0].matchAll(checkForNumbers)];
-  sum += currentNumbers[0][0] * currentNumbers[1][0];
+    if (passingresult[0] == "do()") { proceed = true }
+    else if (passingresult[0] == "don't()") {proceed = false}
+    else {
+        if (proceed) {
+            let currentNumbers = [];
+            currentNumbers = [...passingresult[0].matchAll(checkForNumbers)];
+            
+            sum += parseInt(currentNumbers[0][0]) * parseInt(currentNumbers[1][0]);
+        }
+    }
 });
-
 console.log(sum);
